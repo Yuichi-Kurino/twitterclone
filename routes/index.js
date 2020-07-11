e7f0588f897388c0832d1976c06af400bcba0ac6
YuyusanK <kurinoyuichi@gmail.com>
2020-07-10 17:42:17 -0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,29 @@ router.get('/signin', function(req, res){
   res.render('signin', {title:"sign up"});
 });
 
+router.post('/authenticateUser', async function (req,res) {
+  //check our own database
+  const userInfo = await userModel.findUsersByEmail(req.body.email);
 
+  if(userInfo.length === 0||req.body.password!==userInfo[0].password){
+    res.end("User info incorrect");
+  }
+  else{
+    const userDataPacket = {
+      id:userInfo[0].id,
+      email: userInfo[0].email,
+      password: userInfo[0].password
+    };
+    const token = await tokenUtil.generateToken(userDataPacket);
+    if(!token){
+      res.clearCookie('userToken');
+      res.redirect('/signin');
+    }else {
+      res.clearCookie('userToken');
+      res.cookie("userToken", token, {expire: new Date() + 1});
+      console.log(token);
+      res.end("User Authenticated");
+    }
+  }
+});
 module.exports = router;
